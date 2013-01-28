c43104695790b5ad9053054987f230fbad76d5c6
Gordon Dickens <gordon@gordondickens.com>
2013-01-28 11:03:13 -0500

    removed Guestbook and fixed bad repo uris

--- Guestbook/src/main/resources/com/habuma/guestbook/guest-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Guest (
-	id IDENTITY,
-	NAME VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	dateOfVisit TIMESTAMP NOT NULL
-);
-
-INSERT INTO Guest (NAME, email, dateOfVisit) VALUES ('Craig', 'craig@habuma.com', CURRENT_TIMESTAMP());
